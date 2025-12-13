c7c7cd5789bb0cd848cce098f0da24addfd2f958
tomato <151866114+bryan7hc@users.noreply.github.com>
2025-12-13 00:00:00 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+app.get('/products', (req, res) => {
+    res.json({
+        id: '1',
+        name: "Laptop gaming",
+        price: 1000,
+        description: "laptop gamin lenovo",
+        imageURL:   "https://rimage.ripley.com.pe/home.ripley/Attachment/WOP/1/2004261418923/full_image-2004261418923.jpg"
+    })
+})  
+
+app.listen(port, () => {
+    console.log('server on port ', port)
+})
