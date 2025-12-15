9124d671c8dff64c9936d0b66a4e3df73ebe4587
tomato <151866114+bryan7hc@users.noreply.github.com>
2025-12-14 21:34:07 -0500

    arreglo

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/products', (req, res) => {
-    res.json({
+    res.json([
+        {
         id: '1',
         name: "Laptop gaming",
         price: 1000,
         description: "laptop gamin lenovo",
         imageURL:   "https://rimage.ripley.com.pe/home.ripley/Attachment/WOP/1/2004261418923/full_image-2004261418923.jpg"
-    })
+        }
+    ])
 })  
 
 app.listen(port, () => {
